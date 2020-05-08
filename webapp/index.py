3d31dca09f02569287f72808f8989b16a7469330
rishabhmeswani <rishabhm@berkeley.edu>
2020-05-07 21:17:11 -0700

    Edited all routes, html, final version!

--- webapp/index.py
+++ webapp/index.py
@@ -17,93 +17,77 @@ def datab():
 @app.route("/")
 def index():
    result = db.engine.execute("SELECT DISTINCT(PROFESSION_NAME) FROM Professions")
-   accs = []
+   names = []
 
    for row in result:
        name = {}
-       name["Accomodations"] = row[0]
-       accs.append(name)
+       name["menu"] = row[0]
+       names.append(name)
 
-   return render_template("index.html", data=accs)
-
-
+   return render_template("index.html", data=names)
 
 
 
-@app.route("/destinations", methods=["POST", "GET"])
-def dest():
-
-    #result = db.engine.execute("select * from DEST")
+@app.route("/openings", methods=["POST", "GET"])
+def openings():
 
     if request.method == "GET":
-        search = request.args.get('accomodations')
+        search = request.args.get('opening')
         result = db.engine.execute("SELECT Company_Name, Opening_Description, Profession_Name FROM Professions INNER JOIN Openings ON Professions.Profession_Id = Openings.Profession_Id INNER JOIN Offices O on Openings.Office_Id = O.Office_Id INNER JOIN Companies C on O.Company_Id = C.Company_Id WHERE Profession_Name = %s",search)
 
     else:
-        result = db.engine.execute("select * from DEST")
+        result = db.engine.execute("select * from Openings")
 
 
     names = []
 
     for row in result:
         name = {}
-        name["Dest_Name"] = row[0]
-        name["Dest_Price"] = row[1]
+        name["Company_Name"] = row[0]
+        name["Description"] = row[1]
         names.append(name)
-    return render_template('show_d.html',customers=names)
+    return render_template('show_d.html',data=names)
 
 
-
-@app.route("/destinations_two", methods=["POST", "GET"])
-def dest_two():
-    #result = db.engine.execute("select * from DIVECUST")
+@app.route("/company", methods=["POST", "GET"])
+def company():
+    
     if request.method == "GET":
         search = request.args.get('search')
 
-        result = db.engine.execute("SELECT Person_Name FROM People INNER JOIN Positions P on People.Person_Id = P.Person_Id INNER JOIN Offices O on P.Office_Id = O.Office_Id INNER JOIN Companies C on O.Company_Id = C.Company_Id WHERE Company_Name = %s",search)
+        result = db.engine.execute("SELECT Person_Name, Profession_Name FROM People INNER JOIN Positions P on People.Person_Id = P.Person_Id INNER JOIN Offices O on P.Office_Id = O.Office_Id INNER JOIN Companies C on O.Company_Id = C.Company_Id INNER JOIN Positions on People.Person_Id = Positions.Person_Id INNER JOIN Professions on Positions.Profession_Id = Professions.Profession_Id WHERE Company_Name = %s",search)
     else:
-        result = db.engine.execute("select * from DEST")
+        result = db.engine.execute("select * from Positions")
     names = []
 
     for row in result:
         name = {}
-        name["Cust_Name"] = row[0]
-        #name["City"] = row[1]
-        #name["State"] = row[4]
+        name["Person_Name"] = row[0]
+        name["Profession_Name"] = row[1]
         names.append(name)
 
-    return render_template('show_f.html',customers=names)
+    return render_template('show_f.html',data=names)
 
 
 
-
-
-
-
-
-
-@app.route("/customers", methods=["POST", "GET"])
-def customers():
+@app.route("/salary", methods=["POST", "GET"])
+def salary():
     if request.method == "GET":
-        search = request.args.get('accomodations')
+        search = request.args.get('salary')
         result = db.engine.execute("SELECT Profession_Name, ROUND(AVG(Positions.Salary)) FROM Positions INNER JOIN Professions P on Positions.Profession_Id = P.Profession_Id WHERE Profession_Name = %s",search)
     else:
-        result = db.engine.execute("select * from DEST")
+        result = db.engine.execute("select * from Positions")
 
 
     names = []
 
     for row in result:
         name = {}
-        name["Dest_Name"] = row[0]
-        name["Dest_Price"] = row[1]
+        name["Profession_Type"] = row[0]
+        name["Salary"] = row[1]
         names.append(name)
-    return render_template('show_e.html',customers=names)
+    return render_template('show_e.html',data=names)
 
-@app.route("/customers/<string:name>/")
-def getMember(name):
-   return render_template(
-   'show_c.html',customer=name)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
